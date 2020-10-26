09397879d4644987870d0134e5f0de560d5f0f39
Daniele <danlup94@gmail.com>
2020-10-26 16:18:39 +0100

    Inserimento codice js

---
+++ javascript/myscript.js
@@ -0,0 +1,9 @@
+// User wants to print numbers 1 to 100.
+// Numbers divisible by 3 will read FIZZ,those divisible by 5 will read BUZZ
+
+// print numbers 1 to 100
+
+for (var i = 1; i <= 100; i++) {
+ console.log(i);
+ document.write(i);
+}
