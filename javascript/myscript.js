147e7be3fda784f66913d12dba1b4e50038d3a9c
Daniele <danlup94@gmail.com>
2020-10-26 16:28:11 +0100

    Apertura ticket

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -6,4 +6,10 @@
 for (var i = 1; i <= 100; i++) {
  console.log(i);
  document.write(i);
+ if (i % 3 === 0) {
+   document.write("Fizz")
+
+ }else if (i % 5 === 0) {
+   document.write("Buzz")
+ }
 }
