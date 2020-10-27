d96fe56dc344b0237944a457d8d07be96f8bea81
Daniele <danlup94@gmail.com>
2020-10-27 10:07:28 +0100

    Fix ciclo for

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,15 +1,21 @@
 // User wants to print numbers 1 to 100.
 // Numbers divisible by 3 will read FIZZ,those divisible by 5 will read BUZZ
 
-// print numbers 1 to 100
+// print numbers 1 to 100 defining those divisible for 3 and 5 
 
 for (var i = 1; i <= 100; i++) {
- console.log(i);
- document.write(i);
- if (i % 3 === 0) {
-   document.write("Fizz")
 
- }else if (i % 5 === 0) {
-   document.write("Buzz")
+ if (i % 3 === 0 && i % 5 === 0) {
+   console.log("FizzBuzz");
+
+ } else if (i % 5 === 0) {
+   console.log("Buzz");
+
+ } else if (i % 3 === 0) {
+   console.log("Fizz");
+
+ } else {
+   console.log(i);
  }
+
 }
