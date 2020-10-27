6243c08b2becc0727d13db0a7ca31f8e33f97ae3
Daniele <danlup94@gmail.com>
2020-10-27 10:26:44 +0100

    Inserimento output pagina

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,21 +1,27 @@
 // User wants to print numbers 1 to 100.
 // Numbers divisible by 3 will read FIZZ,those divisible by 5 will read BUZZ
 
-// print numbers 1 to 100 defining those divisible for 3 and 5 
+// print numbers 1 to 100 defining those divisible for 3 and 5
+
+var numero = "";
 
 for (var i = 1; i <= 100; i++) {
 
  if (i % 3 === 0 && i % 5 === 0) {
    console.log("FizzBuzz");
-
+   numero = numero + "<li>" + "FizzBuzz" + "</li>";
  } else if (i % 5 === 0) {
    console.log("Buzz");
-
+   numero = numero + "<li>" + "Buzz" + "</li>";
  } else if (i % 3 === 0) {
    console.log("Fizz");
-
+   numero = numero + "<li>" + "Fizz" + "</li>";
  } else {
    console.log(i);
+   numero = numero + "<li>" + i + "</li>";
  }
 
 }
+
+// html output
+document.getElementById('output').innerHTML = numero;
